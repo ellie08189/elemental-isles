f5cb59b9dca556efbc0cdb996ec2a91095101598
ellieF19 <19-EFea@shsg.org>
2025-08-11 12:22:00 +0100

    making constants

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Hello World")
-name = input("Enter name")
-if name == ("Ellie"):
-    print(name)
+"""Main game loop."""
+
+import pygame
+from constants import width, height, white, black
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Elemental Isles")
+clock = pygame.time.Clock()
+
+
+def main():
+    """Run the game."""
+    running = True
