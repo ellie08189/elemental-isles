7770b7b9a371b6be13d4b12592d1487eb7360c13
ellie08189 <ellie.feagan@gmail.com>
2025-11-14 17:49:06 +0000

    remove fc

--- constants.py
+++ constants.py
@@ -1,10 +1,33 @@
-"""Constants for the game."""
+# Screen
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+# Colors
+BACKGROUND = (189, 244, 250)
+GROUND_COLOR = (72, 201, 32)
+PLAYER_COLOR = (255, 0, 0)
+OBSTACLE_COLOR = (0, 0, 255)
+# Player settings
+PLAYER_RADIUS = 50
+PLAYER_SPEED = 5
+JUMP_STRENGTH = -25
+GRAVITY = 1
+# Ground
+GROUND_Y = 750
+# Obstacle settings
+OBSTACLE_RADIUS = 50
+OBSTACLE_SPEED = -5
 
-WIDTH = 800
-HEIGHT = 600
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-FPS = 60
+# background settings
+BACKGROUND_IMAGE = "assets/forest.png"
+BACKGROUND_SPEED = 5
+BACKGROUND_ACTIVE = True
+
+# title settings
+TITLE_IMAGE = "assets/title_screen.png"
+TITLE_POSITION = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+TITLE_ACTIVE = True
+
+# game over settings
+GAME_OVER_IMAGE = "assets/game_over.png"
+GAME_OVER_POSITION = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+GAME_OVER_ACTIVE = True
